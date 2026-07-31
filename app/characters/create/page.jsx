6f8e8d1788644a0ab101658d836ad8859d22b8e6
조승아 <eodrn7021@gmail.com>
2026-07-31 21:08:18 +0900

    chore: JavaScript 및 Sass 프로젝트 구조 설정

---
+++ app/characters/create/page.jsx
@@ -0,0 +1,7 @@
+import styles from "./create.module.scss";
+
+const CharacterCreatePage = () => {
+  return <main className={styles.page}>캐릭터 생성 페이지</main>;
+};
+
+export default CharacterCreatePage;
---
+++ app/characters/page.jsx
@@ -0,0 +1,7 @@
+import styles from "./characters.module.scss";
+
+const CharactersPage = () => {
+  return <main className={styles.page}>캐릭터 목록 페이지</main>;
+};
+
+export default CharactersPage;
