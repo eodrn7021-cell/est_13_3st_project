e1ce6304701566e7443d12511dc7375d35a7acc6
조승아 <eodrn7021@gmail.com>
2026-08-03 21:57:37 +0900

    fix: 주요 기능 카드 아이콘 채움 스타일 적용

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,6 +37,13 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          precedence="default"
+        />
+      </head>
       <body className={`${Pretendard.variable} ${LoraFont.variable}`}>{children}</body>
     </html>
   );
