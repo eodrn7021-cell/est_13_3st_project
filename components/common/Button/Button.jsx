f3545dcb78ddd892f92b48bcca445853f792f3a7
조승아 <eodrn7021@gmail.com>
2026-08-03 21:06:21 +0900

    feat: 로그인 및 회원가입 버튼 스타일 구현

---
+++ components/common/Button/Button.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import styles from "./Button.module.scss";
+
+const Button = ({
+  children,
+  type = "button",
+  variant = "primary",
+  size = "medium",
+  disabled = false,
+  fullWidth = false,
+  className = "",
+  onClick,
+}) => {
+  const ButtonClassName = [
+    styles.button,
+    styles[`button_${variant}`],
+    styles[`button_${size}`],
+    fullWidth ? styles.button_full_width : "",
+    className,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  return (
+    <button className={ButtonClassName} type={type} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
