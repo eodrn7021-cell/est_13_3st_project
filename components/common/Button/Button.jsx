9af8d8ae0ee93b11d235e56c889f2431cdc6b4c7
chaewon5205 <parkjihae9262@gmail.com>
2026-08-07 15:38:42 +0900

    feat: 새로운 정보 추가

--- components/common/Button/Button.jsx
+++ components/common/Button/Button.jsx
@@ -2,9 +2,9 @@
 
 import styles from "./Button.module.scss";
 
-const Button = ({ children, variant = "primary", type = "button", className = "", ...props }) => {
+const Button = ({ children, type = "button", onClick, disabled = false }) => {
   return (
-    <button type={type} className={`${styles.button} ${styles[variant]} ${className}`} {...props}>
+    <button type={type} className={styles.button} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
