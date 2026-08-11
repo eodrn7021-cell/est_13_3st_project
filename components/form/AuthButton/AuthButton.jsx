0d07685cdd181ac5cbe98b7f047fe460f90e63c3
조승아 <eodrn7021@gmail.com>
2026-08-12 08:03:42 +0900

    fix: 로그인 페이지 UI 및 반응형 수정

--- components/form/AuthButton/AuthButton.jsx
+++ components/form/AuthButton/AuthButton.jsx
@@ -1,13 +1,25 @@
 "use client";
 
-import styles from "./Button.module.scss";
+import styles from "./AuthButton.module.scss";
 
-const Button = ({ children, type = "button", onClick, disabled = false }) => {
+const AuthButton = ({
+  children,
+  type = "button",
+  variant = "primary",
+  onClick,
+  disabled = false,
+  className = "",
+}) => {
+  // 로그인/회원가입 전용 버튼 variant
+  const buttonClassName = [styles.button, styles[`button_${variant}`], className]
+    .filter(Boolean)
+    .join(" ");
+
   return (
-    <button type={type} className={styles.button} onClick={onClick} disabled={disabled}>
+    <button type={type} className={buttonClassName} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
 };
 
-export default Button;
+export default AuthButton;
